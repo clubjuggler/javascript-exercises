af814a41fd52e059f5b8c5edb0588cd94590cf52
clubjuggler <37815719+clubjuggler@users.noreply.github.com>
2023-12-06 13:14:49 +0000

    Fix failing test in reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(stringIn) {
+    let stringArr = stringIn.split('')
 
+    let revStringArr = stringArr.reverse()
+
+    let revString = revStringArr.join('')
+
+    return revString
 };
 
 // Do not edit below this line
