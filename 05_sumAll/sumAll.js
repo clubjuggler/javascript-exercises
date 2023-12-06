5303e880383f0832abf5552aa256ad920b9d3e11
clubjuggler <37815719+clubjuggler@users.noreply.github.com>
2023-12-06 15:39:07 +0000

    Fix tests in sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    // Case when non-numeric
+    if ((typeof start != "number") || (typeof end != "number")) {
+        return "ERROR"
+    }
+
+    // Case when either number is negative
+    if ((start < 0) || (end < 0)) {
+        return "ERROR"
+    }
+
+    let a, b
+    // Case when larger number is first
+    if (start > end){
+        a = end
+        b = start
+    } else {
+        a = start
+        b = end
+    }
+
+    let finalSum = 0
+
+    for (let i = a; i < b+1; i++) {
+        finalSum += i
+    }
+
+    return finalSum
 };
 
 // Do not edit below this line
