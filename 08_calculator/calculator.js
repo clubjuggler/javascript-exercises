68e068dda37ba55624620120b26a48f0dfa2044e
clubjuggler <37815719+clubjuggler@users.noreply.github.com>
2023-12-06 16:46:14 +0000

    Fix tests in calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arrIn) {
+  let s = 0
+
+	for (i = 0; i < arrIn.length; i++) {
+     s += arrIn[i]
+  }
+  return s
 };
 
-const multiply = function() {
+const multiply = function(arrIn) {
+  let p = 1
 
+	for (i = 0; i < arrIn.length; i++) {
+     p *= arrIn[i]
+  }
+  return p
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let p = 1
+  if (a == 0) {
+    return p
+  } else {
+    for (i = 1; i < a +1; i++) {
+      p *= i
+    }
+  };
+	return p
 };
 
 // Do not edit below this line
