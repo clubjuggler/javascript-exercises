7ccc66196ce8a038b574317017a8cc9cc1d73663
clubjuggler <37815719+clubjuggler@users.noreply.github.com>
2023-12-06 13:08:44 +0000

    Fixed failing tests for repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, n) {
+    if (n < 0) {
+        let strOut = 'ERROR'
+        return strOut
+    } else {
+        let strOut = '';
 
+        for (let i = 0; i < n; i++) {
+            strOut += word
+        };
+        return strOut
+    }
 };
 
 // Do not edit below this line
