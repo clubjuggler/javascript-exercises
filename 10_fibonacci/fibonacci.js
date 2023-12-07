e093d858b7679c8fb6d6b4590ec989b9433d30f8
clubjuggler <37815719+clubjuggler@users.noreply.github.com>
2023-12-07 10:16:39 +0000

    Fix tests in fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(idx) {
 
+    if (idx < 0) {
+        return "OOPS"
+    };
+
+    if (typeof idx == 'string') {
+        idx = Number(idx)
+    };
+
+    let initVal = 1;
+    let holdArr = [0,];
+
+    if (idx == 1) {
+        return 1
+    } else {
+
+        // Add first value
+        holdArr.push(initVal);
+
+        for (i = 1; i < idx; i++) {
+            holdArr.push(holdArr[i] + holdArr[i-1])
+        };
+    };
+
+
+    return holdArr.pop();
 };
 
 // Do not edit below this line
