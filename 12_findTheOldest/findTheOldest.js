65c28ebb85bc040036bc0d501395df9a06eeb3e5
clubjuggler <37815719+clubjuggler@users.noreply.github.com>
2023-12-07 14:02:17 +0000

    Fix tests findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // Declare function that finds the larger of two ages
+    const getMax = function(a, b) {
 
+        const aEndDate = getEndDate(a)
+        const bEndDate = getEndDate(b)
+
+        const aAge = aEndDate - a['yearOfBirth']
+        const bAge = bEndDate - b['yearOfBirth']
+        if (aAge>bAge) {
+          return a
+        } else {
+          return b
+        }
+      };
+    
+    const getEndDate = function(person) {
+        const currentYear = new Date().getFullYear()
+        
+        if (typeof person['yearOfDeath'] == 'undefined') {
+            return currentYear
+        } else {
+            return person['yearOfDeath']
+        }
+    }
+    
+    const oldest = people.reduce((acc, currentValue) => getMax(acc, currentValue));
+
+    return oldest
 };
 
 // Do not edit below this line
