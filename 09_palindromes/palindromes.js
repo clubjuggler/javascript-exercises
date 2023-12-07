a8f855790eae99f5ef471fa9a515921214f4231e
clubjuggler <37815719+clubjuggler@users.noreply.github.com>
2023-12-07 09:41:02 +0000

    Fix tests in palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (stringIn) {
 
+    let stringInLower = stringIn.toLowerCase();
+    
+    let stringNoPunc = ''
+
+    //Remove punctuation from orig string
+    for (i = 0; i < stringInLower.length; i++) {
+        if (!!stringInLower[i].match(/^[.,:!? ]/)) {
+            //pass
+        } else {
+            stringNoPunc += stringInLower[i]
+        }
+    }
+
+    let revString = '';
+
+    for (i=stringNoPunc.length - 1; i>-1; i--) {
+        revString += stringNoPunc[i]
+        };
+    
+    //return revString
+    if (stringNoPunc == revString) {
+        return true
+    } else {
+        return false
+    };
 };
 
 // Do not edit below this line
